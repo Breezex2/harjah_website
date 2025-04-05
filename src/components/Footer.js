222379669caa23e521a13bfac0e74e8e9b552f91
Breezex2 <asallah219@gmail.com>
2025-04-05 16:08:29 +0400

    Edited footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,24 @@
 import React from "react";
 import logo from "../assets/img/logo.svg";
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 export const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-left">
         <img src={logo} alt="Company Logo" />
+        <p class="footer-description">
+          “Where Ideas Become Intelligent Solutions.” <br/> We transform your vision
+          into powerful, user-friendly applications designed to elevate your
+          business.
+        </p>
         <ul className="social-contact-icons">
           <li>
-            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <i className="fab fa-facebook-f icon"></i>
             </a>
           </li>
@@ -19,17 +28,26 @@ export const Footer = () => {
             </a>
           </li>
           <li>
-            <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://linkedin.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <i className="fab fa-linkedin-in icon"></i>
             </a>
           </li>
           <li>
-            <a href="https://plus.google.com" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://plus.google.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <i className="fab fa-google-plus-g icon"></i>
             </a>
           </li>
         </ul>
       </div>
+
       <p>Copyright 2024. All Rights Reserved</p>
     </footer>
   );
